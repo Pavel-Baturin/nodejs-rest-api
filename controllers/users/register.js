0a711609831426636f0870fb93bbab6ce1088f43
Pavel-Baturin <pavel.baturin.92@gmail.com>
2022-06-18 17:17:23 +0300

    add send email

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,7 +1,9 @@
 const { User } = require("../../models/user");
 const { Conflict } = require("http-errors");
+const { v4 } = require("uuid");
 const gravatar = require("gravatar");
 const bcrypt = require("bcrypt");
+const { sendEmail } = require("../../helpers");
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
@@ -10,14 +12,27 @@ const register = async (req, res, next) => {
     if (user) {
       throw new Conflict("Email in use");
     }
+    const verificationToken = v4();
     const avatarURL = gravatar.url(email);
     const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    await User.create({ email, password: hashPassword, avatarURL });
+    await User.create({
+      email,
+      password: hashPassword,
+      avatarURL,
+      verificationToken,
+    });
+    const mail = {
+      to: email,
+      subject: "Підтвердження email",
+      html: `<a target="_blank" href="http:localhost:3000/api/users/verify/${verificationToken}">Підтвердіть email</a>`,
+    };
+    await sendEmail(mail);
     res.status(201).json({
       user: {
         email,
         subscription: "starter",
         avatarURL,
+        verificationToken,
       },
     });
   } catch (error) {
